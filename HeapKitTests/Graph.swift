42394c8af60b929c2bcdee2cc62c4829d88345f8
Matt Rajca <matt.rajca@me.com>
2014-12-28 11:52:34 -0600

    Added unit tests

---
+++ HeapKitTests/Graph.swift
@@ -0,0 +1,68 @@
+//
+//  Graph.swift
+//  HeapKit Tests
+//
+//  Copyright (c) 2014 Matt Rajca. All rights reserved.
+//
+
+import Foundation
+
+struct Connection: Equatable, Hashable {
+	let nodeA: Int
+	let nodeB: Int
+	
+	var hashValue: Int {
+		return 65536 * nodeA + nodeB
+	}
+}
+
+func ==(lhs: Connection, rhs: Connection) -> Bool {
+	return lhs.nodeA == rhs.nodeA && lhs.nodeB == rhs.nodeB
+}
+
+class Graph {
+	private let nodes: Dictionary<Int, [Int]>
+	private let weights: Dictionary<Connection, Int>
+	
+	init(nodes: Dictionary<Int, [Int]>, weights: Dictionary<Connection, Int>) {
+		self.nodes = nodes
+		self.weights = weights
+	}
+	
+	func findShortestPaths(fromIndex sourceIndex: Int) -> Dictionary<Int, Int> {
+		var visited = Dictionary<Int, Bool>()
+		
+		var distances = Dictionary<Int, Int>()
+		distances[sourceIndex] = 0
+		
+		let heap = MinHeap<Int, Int>()
+		heap.insert(sourceIndex, weight: 0)
+		
+		while !heap.isEmpty {
+			let index = heap.removeMinimum()!
+			visited[index] = true
+			
+			if let neighbors = nodes[index] {
+				for neighborIndex in neighbors {
+					if visited[neighborIndex] == nil {
+						let weight = weights[Connection(nodeA: index, nodeB: neighborIndex)]!
+						let newDistance = distances[index]! + weight
+						
+						if let currentDistance = distances[neighborIndex] {
+							if newDistance < currentDistance {
+								distances[neighborIndex] = newDistance
+								heap.update(neighborIndex, weight: newDistance)
+							}
+						}
+						else {
+							distances[neighborIndex] = newDistance
+							heap.insert(neighborIndex, weight: newDistance)
+						}
+					}
+				}
+			}
+		}
+		
+		return distances
+	}
+}
--- HeapKitTests/HeapKitTests.swift
+++ HeapKitTests/HeapKitTests.swift
@@ -1,36 +1,69 @@
 //
 //  HeapKitTests.swift
-//  HeapKitTests
+//  HeapKit Tests
 //
-//  Created by Matt on 12/27/14.
 //  Copyright (c) 2014 Matt Rajca. All rights reserved.
 //
 
-import UIKit
 import XCTest
 
 class HeapKitTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+	private var _graph: Graph!
+	
+	private func _loadGraph() -> Graph {
+		let bundle = NSBundle(forClass: HeapKitTests.self)
+		let graphURL = bundle.URLForResource("dijkstraData", withExtension: "txt")
+		XCTAssertNotNil(graphURL, "Cannot find the graph data file")
+		
+		let graphString_ = NSString(contentsOfURL: graphURL!, encoding: NSASCIIStringEncoding, error: nil)
+		XCTAssertNotNil(graphString_, "Cannot load the graph data file")
+		
+		var nodes = Dictionary<Int, [Int]>()
+		var weights = Dictionary<Connection, Int>()
+		
+		if let graphString = graphString_ as? String {
+			graphString.enumerateLines { (line, stop) in
+				let columns = line.componentsSeparatedByString("\t")
+				let index = columns.first!.toInt()!
+				var adjacentIndices = [Int]()
+				
+				for n in 1..<columns.count {
+					let column = columns[n]
+					
+					if column.lengthOfBytesUsingEncoding(NSASCIIStringEncoding) == 0 {
+						continue
+					}
+					
+					let indexWeightTuple = column.componentsSeparatedByString(",")
+					XCTAssertEqual(indexWeightTuple.count, 2, "Should have a pair")
+					
+					let otherIndex = indexWeightTuple.first!.toInt()!
+					let weight = indexWeightTuple.last!.toInt()!
+					
+					adjacentIndices.append(otherIndex)
+					
+					// Make the connection
+					weights[Connection(nodeA: index, nodeB: otherIndex)] = weight
+				}
+				
+				nodes[index] = adjacentIndices
+			}
+		}
+		
+		return Graph(nodes: nodes, weights: weights)
+	}
+	
+	override func setUp() {
+		_graph = _loadGraph()
+	}
+	
+    func testDijkstra() {
+		let paths = _graph.findShortestPaths(fromIndex: 1)
+		
+		let indices = [7, 37, 59, 82, 99, 115, 133, 165, 188, 197]
+		let correctValues = [2599, 2610, 2947, 2052, 2367, 2399, 2029, 2442, 2505, 3068]
+		let values = indices.map { index in paths[index]! }
+		
+		XCTAssertEqual(values, correctValues, "Incorrect distances")
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
